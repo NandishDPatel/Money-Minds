c4b127e50163e542fdc6f18553da9aba57e720f6
NandishDPatel <nandishdpatel22@gmail.com>
2025-02-24 17:32:09 -0600

    budget updation and deletion completed

--- app/(routes)/dashboard/expenses/[id]/page.jsx
+++ app/(routes)/dashboard/expenses/[id]/page.jsx
@@ -7,6 +7,22 @@ import { useUser } from "@clerk/nextjs";
 import BudgetItem from "../../budgets/_components/BudgetItem";
 import AddExpense from "../_components/AddExpense";
 import ExpenseListTable from "../_components/ExpenseListTable";
+import EditBudget from "../_components/EditBudget";
+import { Edit, Pen, PenBox, Trash } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 function ShowExpenses({ params }) {
   // Unwrap the Promise using React.use()
@@ -15,6 +31,7 @@ function ShowExpenses({ params }) {
   const [budgetInfo, setBudgetInfo] = useState(null);
   const [loading, setLoading] = useState(true);
   const [expensesList, setExpensesList] = useState(null);
+  const route = useRouter();
   useEffect(() => {
     if (user && unwrappedParams?.id) {
       getBudgetInfo();
@@ -54,8 +71,8 @@ function ShowExpenses({ params }) {
       .where(eq(Expenses.budgetId, unwrappedParams?.id))
       .orderBy(desc(Expenses.id));
 
-      console.log(result);
-      setExpensesList(result);
+    console.log(result);
+    setExpensesList(result);
   };
 
   if (loading) {
@@ -66,9 +83,59 @@ function ShowExpenses({ params }) {
     return <div>No Budget Information Available</div>;
   }
 
+  const deleteBudget = async () => {
+    const deleteExpenseResult = await db
+      .delete(Expenses)
+      .where(eq(Expenses.budgetId, unwrappedParams?.id))
+      .returning();
+
+    if (deleteExpenseResult) {
+      const result = await db
+        .delete(Budgets)
+        .where(eq(Budgets.id, unwrappedParams?.id))
+        .returning();
+    }
+
+    toast("Budget Deleted Successfully");
+    route.replace("/dashboard/budgets");
+  };
+
   return (
     <div className="p-10">
-      <h2 className="text-2xl font-bold">Expenses</h2>
+      <h2 className="text-2xl font-bold flex justify-between">
+        Expenses
+        <div className="flex gap-2 items-center">
+        <EditBudget budgetInfo={budgetInfo} refreshData={()=>getBudgetInfo()}/>
+        <AlertDialog>
+          <AlertDialogTrigger as child>
+            <Button className="flex gap-2" variant="destructive">
+              <Trash />
+              Delete
+            </Button>
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+              <AlertDialogDescription>
+                This action cannot be undone. This will permanently delete your
+                current budget with all expenses.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction
+                onClick={() => {
+                  deleteBudget();
+                }}
+              >
+                Continue
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+        </div>
+      
+      </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 mt-3 gap-5">
         {budgetInfo ? (
           <BudgetItem budget={budgetInfo} />
@@ -78,13 +145,16 @@ function ShowExpenses({ params }) {
         <AddExpense
           budgetId={unwrappedParams?.id}
           user={user}
-          refreshData={()=>getBudgetInfo()}
+          refreshData={() => getBudgetInfo()}
         />
       </div>
       <div className="mt-4 w-full">
-          <h2 className="font-bold text-lg">Latest Expenses</h2>
-          <ExpenseListTable expensesList={expensesList} refreshData={()=>getBudgetInfo()}/>
-        </div>
+        <h2 className="font-bold text-lg">Latest Expenses</h2>
+        <ExpenseListTable
+          expensesList={expensesList}
+          refreshData={() => getBudgetInfo()}
+        />
+      </div>
     </div>
   );
 }
